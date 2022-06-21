33491c61436971190715aff980c644a1da2db2a2
JustRed23 <manuel.bormans@gmail.com>
2022-06-21 19:22:56 +0200

    Edit some methods

--- src/main/java/dev/JustRed23/LibraryDownloader/Library.java
+++ src/main/java/dev/JustRed23/LibraryDownloader/Library.java
@@ -1,13 +1,19 @@
 package dev.JustRed23.LibraryDownloader;
 
+import dev.JustRed23.LibraryDownloader.utils.MD5;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Library {
 
-    private final String repo;
-    private final String group;
-    private final String artifact;
-    private final String version;
+    public final String repo;
+    public final String group;
+    public final String artifact;
+    public final String version;
+    public final String classifier;
 
-    private final String MD5;
+    private final String md5;
     private final boolean skipMD5;
 
     /**
@@ -20,16 +26,27 @@ public class Library {
      *  Example: 'LibraryDownloader'
      * @param version The version of the library.
      *  Example: '1.0.0'
-     * @param MD5 The MD5 of the library.
+     * @param classifier The classifier of the library.
+     *  Example: '-fatjar'
+     * @param md5 The checksum of the library.
      *  Example: 'b8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f'
      * @param skipMD5 Whether to skip the MD5 check.
      */
-    public Library(String repo, String group, String artifact, String version, String MD5, boolean skipMD5) {
-        this.repo = repo;
+    public Library(String repo, String group, String artifact, String version, String classifier, String md5, boolean skipMD5) {
+        this.repo = repo.endsWith("/") ? repo.substring(0, repo.length() - 1) : repo;
         this.group = group;
         this.artifact = artifact;
         this.version = version;
-        this.MD5 = MD5;
+        this.classifier = classifier;
+        this.md5 = md5;
         this.skipMD5 = skipMD5;
+    }
+
+    public boolean checkMD5(File lib) throws IOException {
+        return skipMD5 || MD5.checkMD5(lib, this.md5);
+    }
+
+    public String getURL() {
+        return repo + "/" + group + "/" + artifact + "/" + version + "/" + artifact + "-" + version + classifier + ".jar";
     }
 }
